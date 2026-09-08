8adf433036e248b818a31f53f69aef6691aa1102
omkarpandarkar72-prog <omkarpandarkar72@gmail.com>
2026-09-08 14:53:03 +0530

    Update general.js

--- router/general.js
+++ router/general.js
@@ -1,46 +1,84 @@
-const axios = require("axios");
+// Get book details based on author
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
 
-async function getAllBooks() {
     try {
-        const response = await axios.get("http://localhost:5000/books");
-        console.log(response.data);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+        const response = await axios.get('http://localhost:5000/');
+        const books = response.data;
 
-async function getBookByISBN(isbn) {
+        const matchingBooks = Object.keys(books)
+            .filter(isbn => books[isbn].author.toLowerCase() === author.toLowerCase())
+            .reduce((acc, isbn) => {
+                acc[isbn] = books[isbn];
+                return acc;
+            }, {});
+
+        if (Object.keys(matchingBooks).length > 0) {
+            return res.status(200).json(matchingBooks);
+        }
+
+        return res.status(404).json({
+            message: `No books found for author '${author}'.`
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error retrieving books'
+        });
+    }
+});
+
+
+// Get book details based on title
+public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title;
+
     try {
-        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
-        console.log(response.data);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+        const response = await axios.get('http://localhost:5000/');
+        const books = response.data;
 
-async function getBooksByAuthor(author) {
+        const matchingBooks = Object.keys(books)
+            .filter(isbn => books[isbn].title.toLowerCase() === title.toLowerCase())
+            .reduce((acc, isbn) => {
+                acc[isbn] = books[isbn];
+                return acc;
+            }, {});
+
+        if (Object.keys(matchingBooks).length > 0) {
+            return res.status(200).json(matchingBooks);
+        }
+
+        return res.status(404).json({
+            message: `No books found with title '${title}'.`
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error retrieving books'
+        });
+    }
+});
+
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+
     try {
-        const response = await axios.get(
-            `http://localhost:5000/author/${encodeURIComponent(author)}`
-        );
-        console.log(response.data);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+        const response = await axios.get('http://localhost:5000/');
+        const books = response.data;
 
-async function getBooksByTitle(title) {
-    try {
-        const response = await axios.get(
-            `http://localhost:5000/title/${encodeURIComponent(title)}`
-        );
-        console.log(response.data);
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+        if (books[isbn]) {
+            return res.status(200).json(books[isbn]);
+        }
 
-getAllBooks();
-getBookByISBN("9780141439518");
-getBooksByAuthor("Jane Austen");
-getBooksByTitle("Pride and Prejudice");
+        return res.status(404).json({
+            message: `No book found with ISBN '${isbn}'.`
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Error retrieving books'
+        });
+    }
+});
